1569e17562e64ea44ec5b5c6bf94fc12577a07e7
Bruno Samuel <brunosamuel.orizona@gmail.com>
2024-02-22 20:25:34 -0300

    Adequações gerais

--- src/services/FilmeManipulador.java
+++ src/services/FilmeManipulador.java
@@ -6,15 +6,11 @@ import java.util.List;
 import java.util.Scanner;
 
 public class FilmeManipulador {
-
-
     private static FilmeManipulador manipulador;
-
 
     public static FilmeManipulador getManipulador(Scanner sc) {
         if (manipulador == null) {
             manipulador = new FilmeManipulador(sc);
-
         }
         return manipulador;
     }
@@ -33,26 +29,28 @@ public class FilmeManipulador {
     public void adicionarFilme(List<Filme> filmes) {
         while (true) {
             System.out.println("Tecle enter sem digitar nada para sair.\n");
-            System.out.println("Nome do filme: ");
+            System.out.print("Nome do filme: ");
             String nome = sc.nextLine();
             if (nome.isBlank()) {
                 System.out.println();
                 break;
             }
 
-            System.out.println("Lançamento do filme: ");
+            System.out.print("Lançamento do filme: ");
             String dataLancamento = sc.nextLine();
-            System.out.println("Orçamento do filme: ");
+            System.out.print("Orçamento do filme: ");
             double orcamento = sc.nextDouble();
             sc.nextLine();
-            System.out.println("Descrição do filme: ");
+            System.out.print("Descrição do filme: ");
             String descricao = sc.nextLine();
             Filme filme = new Filme();
             filme.setNome(nome);
             filme.setDataLancamento(dataLancamento);
             filme.setDescricao(descricao);
             filme.setOrcamento(orcamento);
+            System.out.println();
             diretorManipulador.adicionarDiretores(filme);
+            System.out.println();
             atorManipulador.adicionarAtores(filme);
             filmes.add(filme);
             System.out.println("Filme adicionado com sucesso!");
@@ -67,68 +65,151 @@ public class FilmeManipulador {
         }
 
         while (true) {
-            System.out.println("Digite o nome do filme que deseja editar:\n");
-            String nome = sc.nextLine();
+            System.out.print("Digite o número do ator a editar: ");
 
-            for (Filme filme : filmes) {
-                if (filme.getNome().equals(nome)) {
-                    System.out.println("O que você deseja editar?");
-                    System.out.println("1. Nome");
-                    System.out.println("2. Data de Lançamento");
-                    System.out.println("3. Orçamento");
-                    System.out.println("4. Descrição");
-                    int escolha = sc.nextInt();
-                    sc.nextLine();
-
-                    switch (escolha) {
-                        case 1:
-                            editarNome(filme);
-                            break;
-                        case 2:
-                            editarDataLancamento(filme);
-                            break;
-                        case 3:
-                            editarOrcamento(filme);
-                            break;
-                        case 4:
-                            editarDescricao(filme);
-                            break;
-                        default:
-                            System.out.println("Opção inválida.");
-                    }
-                    System.out.println("Filme editado com sucesso!");
-                    break;
-                }
-            }
-            System.out.println("Deseja editar outro filme? (S/N)");
-            String resposta = sc.nextLine();
-            if (!resposta.equalsIgnoreCase("S")) {
+            int numero = listarELerOpcao(filmes);
+            if (numero == 0) {
                 break;
+            }
+
+            try {
+                this.editarFilme(filmes.get(numero - 1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Número inválido, tente novamente.\n");
+            }
+        }
+
+    }
+
+    private void editarFilme(Filme filme) {
+        String apresentacao = """
+                >>>> Menu - Edição de Filme <<<<
+                      1 - Editar Nome;
+                      2 - Editar data de lançamento;
+                      3 - Editar orçamento;
+                      4 - Editar descrição;
+                      5 - Editar diretores;
+                      6 - Editar atores;
+                      0 - Sair.
+                """;
+        while (true) {
+            System.out.println("Filme");
+            System.out.println("  Nome: " + filme.getNome());
+            System.out.println("  Data de lançamento: " + filme.getDataLancamento());
+            System.out.println("  Orçamento: " + filme.getOrcamento());
+            System.out.println("  Descrição: " + filme.getDescricao());
+            System.out.println("  Diretores: " + filme.getListaDiretores());
+            System.out.println("  Atores: " + filme.getListaAtores());
+            System.out.println(apresentacao);
+
+            System.out.print("> ");
+            char opcao = sc.nextLine().charAt(0);
+            sc.nextLine();
+
+            switch (opcao) {
+                case '1':
+                    editarNome(filme);
+                    break;
+                case '2':
+                    editarDataLancamento(filme);
+                    break;
+                case '3':
+                    editarOrcamento(filme);
+                    break;
+                case '4':
+                    editarDescricao(filme);
+                    break;
+                case '5':
+                    editarDiretores(filme);
+                    break;
+                case '6':
+                    editarAtores(filme);
+                    atorManipulador.editarAtores(filme);
+                    break;
+                case '0':
+                    return;
+                default:
+                    System.out.println("Opção inválida.");
+            }
+            System.out.println("Filme editado com sucesso!");
+        }
+    }
+
+    private void editarDiretores(Filme filme) {
+        String apresentacao = """
+                >>>> Menu - edição de filme (Diretor) <<<<
+                      1 - Adicionar diretores;
+                      2 - Editar diretores;
+                      3 - Deletar diretores;
+                      0 - Sair.
+                """;
+        while (true) {
+            System.out.println(apresentacao);
+            System.out.print("> ");
+            char opcao = sc.nextLine().charAt(0);
+            if (opcao == '0')
+                break;
+
+            if (opcao == '1'){
+                diretorManipulador.adicionarDiretores(filme);
+            } else if (opcao == '2'){
+                diretorManipulador.editarDiretores(filme);
+            } else if (opcao == '3'){
+                diretorManipulador.deletarDiretor(filme);
+            } else {
+                System.out.println("Opção inválida.\n");
+            }
+        }
+    }
+
+    private void editarAtores(Filme filme) {
+        String apresentacao = """
+                >>>> Menu - editão de filme (Ator) <<<<
+                      1 - Adicionar atores;
+                      2 - Editar atores;
+                      3 - Deletar atores;
+                      0 - Sair.
+                """;
+        while (true) {
+            System.out.println(apresentacao);
+            System.out.print("> ");
+            char opcao = sc.nextLine().charAt(0);
+            if (opcao == '0')
+                break;
+
+            if (opcao == '1'){
+                atorManipulador.adicionarAtores(filme);
+            } else if (opcao == '2'){
+                atorManipulador.editarAtores(filme);
+            } else if (opcao == '3'){
+                atorManipulador.deletarAtores(filme);
+            } else {
+                System.out.println("Opção inválida.\n");
             }
         }
     }
 
     private void editarNome(Filme filme) {
-        System.out.println("Insira o novo nome:");
+        System.out.print("Insira o novo nome: ");
         String novoNome = sc.nextLine();
         filme.setNome(novoNome);
     }
 
     private void editarDataLancamento(Filme filme) {
-        System.out.println("Insira a nova data de lançamento:");
+        System.out.print("Insira a nova data de lançamento: ");
         String novaDataLancamento = sc.nextLine();
         filme.setDataLancamento(novaDataLancamento);
     }
 
     private void editarOrcamento(Filme filme) {
-        System.out.println("Insira o novo orçamento:");
+        System.out.print("Insira o novo orçamento: ");
         double novoOrcamento = sc.nextDouble();
         sc.nextLine(); // Limpar o buffer do Scanner
         filme.setOrcamento(novoOrcamento);
     }
 
     private void editarDescricao(Filme filme) {
-        System.out.println("Insira a nova descrição:");
+        System.out.print("Insira a nova descrição: ");
         String novaDescricao = sc.nextLine();
         filme.setDescricao(novaDescricao);
     }
@@ -141,7 +222,7 @@ public class FilmeManipulador {
                 return;
             }
 
-            System.out.println("Digite o número do filme a deletar:\n");
+            System.out.print("Digite o número do filme a deletar:");
 
             int numero = listarELerOpcao(filmes);
             if (numero == 0) {
@@ -153,7 +234,6 @@ public class FilmeManipulador {
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Número inválido, tente novamente.\n");
             }
-
 
         }
     }
